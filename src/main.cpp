b03636092e16b00383960ed2bd28397a65a21d21
Marek <marekfrydrychova@gmail.com>
2026-06-16 21:24:18 -0400

    full initialization of the character DONE

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,28 @@
 namespace fs = std::filesystem;
 using json = nlohmann::json;
 
-void checkSaveFile (const char* homeDir) {
+ void writeSaveFile(std::string userName,const char* homeDir) {
+    fs::path saveDirPath = fs::path(homeDir) / "Documents/repos/RogueLikeGame/save";
+    fs::path saveFilePath = saveDirPath / "saveFile.json";
+    json player;
+    player["username"] = userName;
+    player["level"] = 0;
+    player["stats"]["hp"] = 10;
+    player["stats"]["mp"] = 10;
+    player["stats"]["speed"] = 1;
+    player["stats"]["defense"] = 1;
+
+    std::ofstream saveFile(saveFilePath);
+    if (saveFile.is_open()) {
+        saveFile << player.dump(4);
+        saveFile.close();
+    } else {
+        std::cerr << "Failed to open the file." << std::endl;
+    }
+
+ }
+
+void checkSaveFile (const char* homeDir, std::string userName) {
     fs::path saveDirPath = fs::path(homeDir) / "Documents/repos/RogueLikeGame/save";
     fs::path saveFilePath = saveDirPath / "saveFile.json";
 
@@ -19,30 +40,59 @@ void checkSaveFile (const char* homeDir) {
 
     std::ofstream saveFile(saveFilePath);
     if (saveFile.is_open()) {
+        writeSaveFile(userName, homeDir);
         saveFile.close();
     } else {
         std::cerr << "Failed to create the file." << std::endl;
     }
 }
- void readSaveFile() {
 
+std::tuple<std::string, int, int, int, int, int> readSaveFile(const char* homeDir) {
+    fs::path saveDirPath = fs::path(homeDir) / "Documents/repos/RogueLikeGame/save";
+    fs::path saveFilePath = saveDirPath / "saveFile.json";
+
+    std::ifstream saveFile(saveFilePath);
+
+    if (!saveFile.is_open()) {
+        std::cerr << "could not open save file." << std::endl;
+    }
+
+    json data = json::parse(saveFile);
+    saveFile.close();
+
+    std::string username = data["username"];
+    int level = data["level"];
+    int health = data["stats"]["hp"];
+    int mana = data["stats"]["mp"];
+    int speed = data["stats"]["speed"];
+    int defense = data["stats"]["defense"];
+
+    return std::make_tuple(username, level, health, mana, speed, defense);
  }
 
- void writeSaveFile() {
 
- }
 
 int main(int argc, char* argv[]) {
+    //getting variables to setup the save directory and file to save progress
     const char* homeDir = std::getenv("HOME");
-    fs::path("saveFile.json");
+    fs::path saveFile("saveFile.json");
     if (!homeDir) {
         std::cerr << "Could not find home directory." << std::endl;
         return 1;
     }
 
-    checkSaveFile(homeDir);
+    
 
+    //asking user for username
+    std::string userName;
+    std::cout << "What is your username: ";;
+    std::cin >> userName;
+    //create save file if it doesn't exist and check if it can be created
+    checkSaveFile(homeDir, userName);
+    //read save file and store values in variables
+    auto [username, level, health, mana, speed, defense] = readSaveFile(homeDir);
 
+    
 
     return 0;
 }
