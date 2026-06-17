2abd316a57243ffe2f9efbba0b663e80d637d566
Marek <marekfrydrychova@gmail.com>
2026-06-16 20:44:51 -0400

    added json compatibility and changed header extention to hpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <filesystem>
 #include <fstream>
 #include <cstdlib>
+#include <string>
+#include <vector>
+#include "../include/json.hpp"
 
 namespace fs = std::filesystem;
+using json = nlohmann::json;
 
 void checkSaveFile (const char* homeDir) {
     fs::path saveDirPath = fs::path(homeDir) / "Documents/repos/RogueLikeGame/save";
-    fs::path saveFilePath = saveDirPath / "saveFile.txt";
+    fs::path saveFilePath = saveDirPath / "saveFile.json";
 
     if (!fs::exists(saveDirPath)) {
         fs::create_directories(saveDirPath);
@@ -20,9 +24,17 @@ void checkSaveFile (const char* homeDir) {
         std::cerr << "Failed to create the file." << std::endl;
     }
 }
+ void readSaveFile() {
+
+ }
+
+ void writeSaveFile() {
+
+ }
 
 int main(int argc, char* argv[]) {
     const char* homeDir = std::getenv("HOME");
+    fs::path("saveFile.json");
     if (!homeDir) {
         std::cerr << "Could not find home directory." << std::endl;
         return 1;
